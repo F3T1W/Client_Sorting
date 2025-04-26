4dd01a68396fed3c5a35bdd42a7abb590a5c5480
Vlad Matiushin <particular0010abyss@gmail.com>
2025-04-26 23:37:56 +0300

    Refactored 👺

--- Program.cs
+++ Program.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 
-namespace ClientSort
+namespace ClientSort;
+
+internal static class Program
 {
-    class Program
+    private static void Main(string[] args)
     {
-            static void Main(string[] args)
-            {
-                List<Patient> patient = new List<Patient>() {
-                new Patient("Alex", "Mitchel", 21, "critical"),
-                new Patient("Alex", "Mitchelg", 21, "critical"),
-                new Patient("Kate","Pupsvel", 18, "critical"),
-                new Patient("Kateline","Pupsvel", 18, "critical"),
-                new Patient("Georgy", "Robbinson", 42, "normal"),
-                new Patient("LG", "Company", 120, "normal"),
-                new Patient("Mikky", "Mouse", 30, "critical") };
+        var patient = new List<Patient>() {
+            new Patient("Alex", "Mitchel", 21, "critical"),
+            new Patient("Alex", "Mitchelg", 21, "critical"),
+            new Patient("Kate","Pupsvel", 18, "critical"),
+            new Patient("Kateline","Pupsvel", 18, "critical"),
+            new Patient("Georgy", "Robbinson", 42, "normal"),
+            new Patient("LG", "Company", 120, "normal"),
+            new Patient("Mikky", "Mouse", 30, "critical") };
 
-                Patient.FindByName(patient);
-                Patient.FindBySurname(patient);
-                Patient.Critical(patient);
-                Patient.LessThan(patient);
-            }
-        
+        Patient.FindByName(patient);
+        Patient.FindBySurname(patient);
+        Patient.Critical(patient);
+        Patient.LessThan(patient);
     }
 }
